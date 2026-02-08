796f38a071064af3c5a0b0c7ff70950f9a347e3b
Soham0095 <sohphantom123@gmail.com>
2026-02-09 00:15:58 +0530

    AOP-without exception Handling -->incorrect COMMIT

--- src/main/java/com/example/service/TransferServiceImpl.java
+++ src/main/java/com/example/service/TransferServiceImpl.java
@@ -44,27 +44,30 @@ public class TransferServiceImpl implements TransferService{
 
     @Override
     public void transfer(TransferRequestDto transferRequestDto) {
+        isValidTransfer(transferRequestDto);
+        executeTransfer(transferRequestDto);
+
         // create a transaction log - AOP
-        TransactionLog transactionLog = new TransactionLog(
-                transferRequestDto.fromAccountId(),
-                transferRequestDto.toAccountId(),
-                transferRequestDto.amount()
-        );
-        try {
-            // validate transfer - AOP
-            isValidTransfer(transferRequestDto);
-            //execute func
-            transactionLog.setStatus(TransactionStatus.SUCCESS);
-
-
-            // exception handling and logging - AOP
-        } catch (Exception e){
-            transactionLog.setStatus(TransactionStatus.FAILURE);
-            transactionLog.setFailureReason(e.getMessage());
-            throw e;
-        }
-        finally {
-            System.out.println(transactionLog);
-        }
+//        TransactionLog transactionLog = new TransactionLog(
+//                transferRequestDto.fromAccountId(),
+//                transferRequestDto.toAccountId(),
+//                transferRequestDto.amount()
+//        );
+//        try {
+//            // validate transfer - AOP
+//            isValidTransfer(transferRequestDto);
+//            //execute func
+//            transactionLog.setStatus(TransactionStatus.SUCCESS);
+//
+//
+//            // exception handling and logging - AOP
+//        } catch (Exception e){
+//            transactionLog.setStatus(TransactionStatus.FAILURE);
+//            transactionLog.setFailureReason(e.getMessage());
+//            throw e;
+//        }
+//        finally {
+//            System.out.println(transactionLog);
+//        }
     }
 }
